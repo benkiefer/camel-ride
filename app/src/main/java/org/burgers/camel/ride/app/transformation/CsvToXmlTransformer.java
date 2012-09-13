3ef9968054475ae23f7cce13814a081ca6ba0064
Ben Kiefer <kieferfam@msn.com>
2012-09-12 21:51:06 -0500

    Ben: WIP working on marshalling form a csv to xml

---
+++ app/src/main/java/org/burgers/camel/ride/app/transformation/CsvToXmlTransformer.java
@@ -0,0 +1,28 @@
+package org.burgers.camel.ride.app.transformation;
+
+import com.thoughtworks.xstream.XStream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.spel.ast.StringLiteral;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvToXmlTransformer {
+    private XStream xStream;
+
+    public List<String> convert(List<List<String>> csvRows){
+        List<String> resultingXml = new ArrayList<String>();
+        for (List<String> csvRow : csvRows) {
+            Person person = new Person();
+            person.setFirstName(csvRow.get(0));
+            person.setLastName(csvRow.get(1));
+            resultingXml.add(xStream.toXML(person));
+        }
+        return resultingXml;
+    }
+
+    public void setXstream(XStream xstream) {
+        this.xStream = xstream;
+    }
+}
