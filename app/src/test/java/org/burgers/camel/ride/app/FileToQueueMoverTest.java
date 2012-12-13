d63a0bda58dd12754c3008f1532ef42762ea8acf
Ben Kiefer <kieferfam@msn.com>
2012-12-13 07:19:35 -0600

    Ben: cleaning up unnecessary activemq configuration since my endpoint is named activemq

--- app/src/test/java/org/burgers/camel/ride/app/FileToQueueMoverTest.java
+++ app/src/test/java/org/burgers/camel/ride/app/FileToQueueMoverTest.java
@@ -7,9 +7,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Value;
 
+import javax.jms.JMSException;
 import java.io.File;
+import java.net.URISyntaxException;
 
-import static org.junit.Assert.assertEquals;
+import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class FileToQueueMoverTest extends CamelFileBasedTestCase {
@@ -19,15 +21,15 @@ public class FileToQueueMoverTest extends CamelFileBasedTestCase {
     @Value("${camel.ride.app.queue.destination}")
     private String destination;
 
-    final int[] count = {0};
+    private CountingProcessor processor;
 
     @Before
     public void setup(){
-        FileUtil.makeOrCleanDirectory(inputDirectory);
+        processor = new CountingProcessor();
     }
 
     @Test
-    public void route() {
+    public void route() throws JMSException, URISyntaxException {
         registerDestination();
         loadFileToProcess("test.txt", "text");
         runCamelAndWaitForItToFinish();
@@ -40,7 +42,7 @@ public class FileToQueueMoverTest extends CamelFileBasedTestCase {
     }
 
     private void assertDestinationContainsCount(int expected) {
-        assertEquals(expected, count[0]);
+        assertEquals(expected, processor.getCount());
     }
 
     private void registerDestination() {
@@ -48,16 +50,24 @@ public class FileToQueueMoverTest extends CamelFileBasedTestCase {
             getCamelContext().addRoutes(new RouteBuilder() {
                 @Override
                 public void configure() throws Exception {
-                    from(destination).process(new Processor() {
-                        @Override
-                        public void process(Exchange exchange) throws Exception {
-                            count[0] += 1;
-                        }
-                    });
+                    from(destination).process(processor);
                 }
             });
         } catch (Exception e){
             fail("Failed because: " + e.getMessage());
+        }
+    }
+
+    class CountingProcessor implements Processor {
+        private int count = 0;
+
+        @Override
+        public void process(Exchange exchange) throws Exception {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
         }
     }
 
