c4195192fabb5de29f4a3bd7d2e5c69885d03043
Ben Kiefer <kieferfam@msn.com>
2012-12-15 17:00:55 -0600

    Ben: adding more comments to the route to make it more readable and cutting it over to stream the results

--- app/src/main/java/org/burgers/camel/ride/app/FileMover.java
+++ app/src/main/java/org/burgers/camel/ride/app/FileMover.java
@@ -6,16 +6,24 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class FileMover extends SpringRouteBuilder {
-    private @Value("${camel.ride.app.output.directory}") String outputDirectory;
-    private @Value("${camel.ride.app.input.directory}") String inputDirectory;
+    private
+    @Value("${camel.ride.app.output.directory}")
+    String outputDirectory;
+    private
+    @Value("${camel.ride.app.input.directory}")
+    String inputDirectory;
 
     @Override
     public void configure() throws Exception {
         from("file:" + inputDirectory)
-                .multicast()
-                    .to("file:" + outputDirectory)
-                    .pipeline()
-                        .unmarshal().csv().beanRef("csvToPersonProcessor", "process");
+                // send a copy of the file to my output directory
+                .to("file:" + outputDirectory)
+                    // break each row of the file into a separate record and send it to the csv processor
+                    .split(body().tokenize("\n")).streaming()
+                    // breaks the csv row into a list of one list of strings
+                    .unmarshal().csv()
+                    // send it to my processor
+                    .beanRef("csvToPersonProcessor", "process");
     }
 
 }
