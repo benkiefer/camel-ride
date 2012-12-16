d16bad2681b9b2957fbff20f508a3cad5b2060ef
Ben Kiefer <kieferfam@msn.com>
2012-12-16 04:04:45 -0600

    Ben: breaking out the csv marshalling process so that you can easily flip the CsvDataFormat parameters if you want to play with the code at all

--- app/src/main/java/org/burgers/camel/ride/app/FileMover.java
+++ app/src/main/java/org/burgers/camel/ride/app/FileMover.java
@@ -1,27 +1,30 @@
 package org.burgers.camel.ride.app;
 
+import org.apache.camel.dataformat.csv.CsvDataFormat;
 import org.apache.camel.spring.SpringRouteBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FileMover extends SpringRouteBuilder {
-    private
     @Value("${camel.ride.app.output.directory}")
-    String outputDirectory;
-    private
+    private String outputDirectory;
+
     @Value("${camel.ride.app.input.directory}")
-    String inputDirectory;
+    private String inputDirectory;
 
     @Override
     public void configure() throws Exception {
+        CsvDataFormat format = new CsvDataFormat();
+        format.setSkipFirstLine(false);
+
         from("file:" + inputDirectory)
                 // send a copy of the file to my output directory
                 .to("file:" + outputDirectory)
                     // break each row of the file into a separate record and send it to the csv processor
                     .split(body().tokenize("\n")).streaming()
                     // breaks the csv row into a list of one list of strings
-                    .unmarshal().csv()
+                    .unmarshal(format)
                     // send it to my processor
                     .beanRef("csvToPersonProcessor", "process");
     }
